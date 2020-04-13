be1f400ab5cdfca13fad063844b038aec9d850fb
somashekhar <37308901+som-kanade@users.noreply.github.com>
2020-04-13 20:59:45 +0530

    first commit

---
+++ snapshots_older_than30days.py
@@ -0,0 +1,11 @@
+# list snapshots which are older than 30 days
+import boto3
+import pytz
+from datetime import datetime, timedelta
+
+ec2_client=boto3.client('ec2',region_name='us-east-1')
+snapshots=ec2_client.describe_snapshots(OwnerIds=['self'])['Snapshots']
+oldest_date = datetime.now(pytz.utc) - timedelta(days=1)
+for snaps in snapshots:
+    if(snaps['StartTime'] < oldest_date):
+        print(snaps['SnapshotId'])
