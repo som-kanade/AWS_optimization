7892762f3d52e39a02144504114c8aea07bde79c
somashekhar <37308901+som-kanade@users.noreply.github.com>
2020-04-15 15:02:11 +0530

    Update snapshots_older_than30days.py

--- snapshots_older_than30days.py
+++ snapshots_older_than30days.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta
 
 ec2_client=boto3.client('ec2',region_name='us-east-1')
 snapshots=ec2_client.describe_snapshots(OwnerIds=['self'])['Snapshots']
-oldest_date = datetime.now(pytz.utc) - timedelta(days=1)
+oldest_date = datetime.now(pytz.utc) - timedelta(days=30)
 for snaps in snapshots:
     if(snaps['StartTime'] < oldest_date):
          print(snaps['SnapshotId'], 'has volume of', snaps['VolumeSize'])
